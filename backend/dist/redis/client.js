33e350637c78e22b29d1ec5ecdd608294a840764
Owen TM <oturnermajor@gmail.com>
2016-04-03 12:08:50 +0100

    heroku

--- backend/dist/redis/client.js
+++ backend/dist/redis/client.js
@@ -10,7 +10,7 @@ var _redis2 = _interopRequireDefault(_redis);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var DB_URL = process.env.REDIS_URL || 'redis://localhost:6379';
+var DB_URL = process.env.REDISTOGO_URL || 'redis://localhost:6379';
 var DB_NUM = process.env.REDIS_DB || 0;
 var client = _redis2.default.createClient(DB_URL);
 
--- backend/lib/redis/client.js
+++ backend/lib/redis/client.js
@@ -1,6 +1,6 @@
 import redis from 'redis'
 
-const DB_URL = process.env.REDIS_URL || 'redis://localhost:6379'
+const DB_URL = process.env.REDISTOGO_URL || 'redis://localhost:6379'
 const DB_NUM = process.env.REDIS_DB || 0
 const client = redis.createClient(DB_URL)
 
